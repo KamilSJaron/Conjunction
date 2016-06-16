d9dab20c3522faa8b126a0d541d8c6708542006f
Kamil S Jaroň <376090@mail.muni.cz>
2016-06-16 22:43:20 +0200

    GNU GPL 3 licences everywhere

--- makefile
+++ makefile
@@ -2,8 +2,8 @@ CXX = g++
 CXXFLAGS = -std=c++11 -O3 -Wall -pedantic
 
 
-PRG = forsim
-TEST = forsim_test
+PRG = conjunction
+TEST = conjunction_test
 CLASSES = ./src/Chromosome.o ./src/Individual.o ./src/Imigrant.o ./src/Deme.o ./src/SelectionModel.o ./src/World.o
 OBJ = ./src/main.o $(CLASSES)
 
@@ -20,7 +20,7 @@ test: $(TEST)
 	rm $(TEST)
 
 $(TEST): $(CLASSES)
-	$(CXX) $(CXXFLAGS) -o $(TEST) ./test/forsim_test.cpp $(CLASSES)
+	$(CXX) $(CXXFLAGS) -o $(TEST) ./test/conjunction_test.cpp $(CLASSES)
 
 clean:
 	rm -f $(PRG) $(OBJ) $(TEST)
--- src/Chromosome.cpp
+++ src/Chromosome.cpp
@@ -1,3 +1,21 @@
+/*
+Implemenation of class Chromosome. Contating a map between junctions and origin of genetic materials. 
+Copyright (C) 2014-2016  Kamil S. Jaron
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <iostream>
 #include <map>
 #include <vector>
@@ -26,7 +44,7 @@ Chromosome::~Chromosome(){
 }
 
 /* PLOTTING METHODS */
-void Chromosome::showChromosome() const{
+void Chromosome::showCh§romosome() const{
 	for(map<int, char>::const_iterator i=chromosome.begin(); i!=chromosome.end(); ++i){
 		cout << i->first << ':' << i->second << '\n';
 	}
